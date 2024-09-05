672903e7a280be2db7622ce3883b00fdc96e6b79
kody.low <kodylow7@gmail.com>
2024-09-05 16:20:26 -0700

    fix: fix

--- scripts/update-ui.sh
+++ scripts/update-ui.sh
@@ -8,7 +8,7 @@ if [ -z "$version" ] || ! [[ $version =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
 fi
 
 find configurations -name docker-compose.yaml | while read -r file; do
-  sed -i '' "s|image: fedimintui/guardian-ui:[0-9.]*|image: fedimintui/guardian-ui:v$version|g" "$file"
-  sed -i '' "s|image: fedimintui/gateway-ui:[0-9.]*|image: fedimintui/gateway-ui:v$version|g" "$file"
+  sed -i '' "s|image: fedimintui/guardian-ui:[0-9.]*|image: fedimintui/guardian-ui:$version|g" "$file"
+  sed -i '' "s|image: fedimintui/gateway-ui:[0-9.]*|image: fedimintui/gateway-ui:$version|g" "$file"
   echo "Updated Guardian and Gateway UI version to $version in $file"
 done
